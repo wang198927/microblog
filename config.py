5c4db80e767c7ae49dc9820e1f2278e083fcc82c
wdm <wang198927@163.com>
2016-01-07 22:07:17 +0800

    model层基本完成

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 __author__ = 'DD_2'
+import os
+
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
@@ -8,3 +10,9 @@ OPENID_PROVIDERS = [
     { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
